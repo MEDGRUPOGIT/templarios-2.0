4f40d47d456fbc1bf570a162b0a1d52910f6042e
yure-nogueira <pedro.nogueira@terceirizados.medgrupo.com.br>
2024-12-26 16:51:53 -0300

    atualização interactions - mask animation

--- interactions/animations/compositions/mask.animations.d.ts
+++ interactions/animations/compositions/mask.animations.d.ts
@@ -1,4 +1,4 @@
 import { SlideDirection } from '../../enums/slide-direction.enums';
 export declare namespace MaskAnimations {
-    function mask(elements: Element | Element[] | Node | Node[] | NodeList, direction: keyof typeof SlideDirection): import("@ionic/core").Animation;
+    function mask(elements: Element | Element[] | Node | Node[] | NodeList, direction: SlideDirection): import("@ionic/core").Animation;
 }

37561ceb9183ddb665ced1a82d42b322586c1e0a
yure-nogueira <pedro.nogueira@terceirizados.medgrupo.com.br>
2025-03-10 16:27:02 -0300

    hash icons e ang material 14

---
+++ icons/utilities/index.d.ts
@@ -0,0 +1,4 @@
+export * from './utilities/constants/icon-defs.constant';
+export * from './utilities/constants/icon.constant';
+export * from './utilities/enums/icon-defs.enum';
+export * from './utilities/enums/icon.enum';
